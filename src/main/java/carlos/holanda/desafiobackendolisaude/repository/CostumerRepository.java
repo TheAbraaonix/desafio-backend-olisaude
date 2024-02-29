2b7b97b210558303ce4194810c5b40de150f7ce9
TheAbraaonix <carlosabraao@hotmail.com.br>
2024-02-29 16:44:02 -0300

    sobrescrita do método findAll() do JPA

--- src/main/java/carlos/holanda/desafiobackendolisaude/repository/CostumerRepository.java
+++ src/main/java/carlos/holanda/desafiobackendolisaude/repository/CostumerRepository.java
@@ -1,9 +1,17 @@
 package carlos.holanda.desafiobackendolisaude.repository;
 
 import carlos.holanda.desafiobackendolisaude.model.Costumer;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CostumerRepository extends JpaRepository<Costumer, Long> {
+    @Override
+    default List<Costumer> findAll() {
+        List<Costumer> orderedCostumers = findAll(Sort.by("name").ascending());
+        return orderedCostumers;
+    }
 }
