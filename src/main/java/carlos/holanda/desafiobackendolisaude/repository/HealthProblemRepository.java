9b3e0eac3fd8c7af36839ac8cc137111626d115f
TheAbraaonix <carlosabraao@hotmail.com.br>
2024-02-29 16:22:40 -0300

    alterando HealthProblemRepository

--- src/main/java/carlos/holanda/desafiobackendolisaude/repository/HealthProblemRepository.java
+++ src/main/java/carlos/holanda/desafiobackendolisaude/repository/HealthProblemRepository.java
@@ -2,6 +2,8 @@ package carlos.holanda.desafiobackendolisaude.repository;
 
 import carlos.holanda.desafiobackendolisaude.model.HealthProblem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface HealthProblemRepository extends JpaRepository<HealthProblem, Long> {
 }
