b81140c5092847188a81da2107352f48ecdc7097
TheAbraaonix <carlosabraao@hotmail.com.br>
2024-02-29 10:57:47 -0300

    setup projeto

---
+++ src/main/java/carlos/holanda/desafiobackendolisaude/DesafioBackendOlisaudeApplication.java
@@ -0,0 +1,13 @@
+package carlos.holanda.desafiobackendolisaude;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioBackendOlisaudeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioBackendOlisaudeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/carlos/holanda/desafiobackendolisaude/DesafioBackendOlisaudeApplicationTests.java
@@ -0,0 +1,13 @@
+package carlos.holanda.desafiobackendolisaude;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioBackendOlisaudeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
